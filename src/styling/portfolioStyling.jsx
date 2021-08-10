4274039d4abf1fa6092e5547f59ff1797d114f51
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-08-10 12:23:11 +0200

    some more changes

--- src/styling/portfolioStyling.jsx
+++ src/styling/portfolioStyling.jsx
@@ -4,7 +4,8 @@ export const ProjectsWrapper = styled.section`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-`
+  overflow: hidden;
+`;
 
 export const Project = styled.article`
   margin: 50px;
@@ -19,25 +20,25 @@ export const Project = styled.article`
   box-shadow: 0 10px 10px #707070;
   transition: box-shadow 0.5s ease;
 
-  &:hover{
-    box-shadow: 5px 10px 10px  #707070;
-    }
+  &:hover {
+    box-shadow: 5px 10px 10px #707070;
+  }
 
-   @media (max-width: 899px){
-     height: auto;
-   }
+  @media (max-width: 899px) {
+    height: auto;
+  }
 
-   @media (max-width: 535px){
-     min-width: 80%;
-   }
-`
+  @media (max-width: 535px) {
+    min-width: 80%;
+  }
+`;
 
 export const Heading = styled.h1`
   font-weight: bold;
   font-size: 40px;
-  font-family: 'Squada One', cursive;
+  font-family: "Squada One", cursive;
   text-align: center;
-`
+`;
 
 export const Tag = styled.p`
   font-weight: bold;
@@ -47,38 +48,38 @@ export const Tag = styled.p`
   border-radius: 3px;
   margin-right: 10px;
   margin-top: 5px;
-`
+`;
 export const Tags = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin-top: 15px; 
-  justify-content: center;  
-`
+  margin-top: 15px;
+  justify-content: center;
+`;
 export const ProjectLink = styled.a`
   text-decoration: none;
   color: #000;
-`
+`;
 export const Title = styled.h2`
-   position: absolute;
-   color: grey;
-   text-shadow: 3px 3px #51484f;
-    top: 20%;
-    left: 20%;
-    font-size: 30px;
-    font-weight: bold;  
-    font-family: 'RocknRoll One', sans-serif;
-`
+  position: absolute;
+  color: grey;
+  text-shadow: 3px 3px #51484f;
+  top: 20%;
+  left: 20%;
+  font-size: 30px;
+  font-weight: bold;
+  font-family: "RocknRoll One", sans-serif;
+`;
 
 export const Img = styled.img`
-  height: 180px; 
+  height: 180px;
   width: 100%;
 
-  &:hover{
-  -webkit-filter: brightness(.5);
-   filter: brightness(.5);
-  } 
-`
+  &:hover {
+    -webkit-filter: brightness(0.5);
+    filter: brightness(0.5);
+  }
+`;
 export const Description = styled.p`
   margin-top: 10px;
   font-size: 15px;
-`
+`;
